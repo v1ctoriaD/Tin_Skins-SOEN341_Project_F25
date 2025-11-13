ec9fb1c9d3ff7f39929bb2c926dfcac914566bfb
Daniel Ganchev <danielganchev649@gmail.com>
2025-11-13 15:25:53 -0500

    CI fix - database/prisma.js

--- backend/database/prisma.js
+++ backend/database/prisma.js
@@ -1,5 +1,11 @@
-import { PrismaClient } from '../generated/prisma/index.js';
+// import { PrismaClient } from '../generated/prisma/index.js';
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+// database/prisma.js
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
 export default prisma;
